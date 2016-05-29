c535a2870a123e14d7d6f881da0b2812170e2696
heshanindika <k.heshan.indika@gmail.com>
2016-05-29 10:23:18 +0530

    add concurrency info to resource obj

--- ResourceMonitor.js
+++ ResourceMonitor.js
@@ -1,7 +1,71 @@
 ﻿var util = require('util');
-var EventEmiter = require('events').EventEmitter;
+var EventEmitter = require('events').EventEmitter;
 var resourceHandler = require('dvp-ardscommon/ResourceHandler.js');
+var redisHandler = require('dvp-ardscommon/RedisHandler.js');
 
+var ProcessResourceData = function(logkey,resource, callback){
+    //var e = new EventEmitter();
+    //process.nextTick(function () {
+        if (resource) {
+            //var count = 0;
+            //for (var i in resourcelist) {
+                //var resource = resourcelist[i].Obj;
+                var companyStr =resource.Company.toString();
+                var tenantStr =resource.Tenant.toString();
+                var concurrencyTags = ["company_" + companyStr, "tenant_" + tenantStr, "resourceid_"+resource.ResourceId,"objtype_ConcurrencyInfo"];
+                redisHandler.SearchObj_T(logkey,concurrencyTags,function(cErr, cResult){
+                        var tempConcurrencyInfos = [];
+                        var pcd = ProcessConcurrencyData(logkey,cResult);
+                        pcd.on('concurrencyInfo', function (obj) {
+                            tempConcurrencyInfos.push(obj);
+                        });
+                        pcd.on('endConcurrencyInfo', function () {
+                            //count++;
+                            resource.ConcurrencyInfo = tempConcurrencyInfos;
+                            callback(resource);
+                        });
+                });
+            //}
+        }
+        else {
+            //e.emit('endResourceInfo');
+            callback(resource);
+        }
+    //});
+
+    //return (e);
+};
+var ProcessCsData = function(logkey, concurrencyInfo, callback){
+    var csTags = ["company_" + concurrencyInfo.Company.toString(), "tenant_" + concurrencyInfo.Tenant.toString(),"handlingType_"+concurrencyInfo.HandlingType, "resourceid_"+concurrencyInfo.ResourceId,"objtype_CSlotInfo"];
+    redisHandler.SearchObj_T(logkey,csTags,function(csErr, csResult){
+        concurrencyInfo.SlotInfo = csResult;
+        callback(concurrencyInfo);
+    });
+};
+var ProcessConcurrencyData = function(logkey,concurrencyInfos){
+    var e = new EventEmitter();
+    process.nextTick(function () {
+        if (Array.isArray(concurrencyInfos) && concurrencyInfos.length>0) {
+            var count = 0;
+            for (var i in concurrencyInfos) {
+                var concurrencyInfo = concurrencyInfos[i];
+                //var csTags = ["company_" + concurrencyInfo.Company.toString(), "tenant_" + concurrencyInfo.Tenant.toString(),"handlingType_"+concurrencyInfo.HandlingType, "resourceid_"+concurrencyInfo.ResourceId,"objtype_CSlotInfo"];
+                ProcessCsData(logkey,concurrencyInfo,function(concurrencyInfo){
+                    count++;
+                    e.emit('concurrencyInfo', concurrencyInfo);
+                    if (concurrencyInfos.length === count) {
+                        e.emit('endConcurrencyInfo');
+                    }
+                });
+            }
+        }
+        else {
+            e.emit('endConcurrencyInfo');
+        }
+    });
+
+    return (e);
+};
 var SearchResourceByTags = function (logkey, searchTags, callback) {
     resourceHandler.SearchResourcebyTags(logkey, searchTags, function (err, resourcelist) {
         if (err) {
@@ -9,13 +73,25 @@ var SearchResourceByTags = function (logkey, searchTags, callback) {
             callback(err, []);
         }
         else {
-            var returnlist = [];
-            if (resourcelist.length > 0) {
-                callback(null, resourcelist);
+            var tempResourceInfos = [];
+            var count = 0;
+            for(var i in resourcelist){
+                var resource = resourcelist[i].Obj;
+                ProcessResourceData(logkey,resource, function(tempResource){
+                    count++;
+                    tempResourceInfos.push(tempResource);
+                    if(count == resourcelist.length) {
+                        callback(null, tempResourceInfos);
+                    }
+                });
             }
-            else {
-                callback(null, resourcelist);
-            }
+            //var pcd = ProcessResourceData(logkey,resourcelist);
+            //pcd.on('resourceInfo', function (obj) {
+            //    tempResourceInfos.push(obj);
+            //});
+            //pcd.on('endResourceInfo', function () {
+            //    callback(null, tempResourceInfos);
+            //});
         }
     });
 };
