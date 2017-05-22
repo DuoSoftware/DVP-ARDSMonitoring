3e9b9981284460960a20005a6364df12f95d9ebf
heshanindika <k.heshan.indika@gmail.com>
2017-05-22 17:46:49 +0530

    calculate call center performance date wise

--- CallCenterMonitor.js
+++ CallCenterMonitor.js
@@ -8,7 +8,9 @@ var logger = require('dvp-common/LogHandler/CommonLogHandler.js').logger;
 var messageFormatter = require('dvp-common/CommonMessageGenerator/ClientMessageJsonFormatter.js');
 var Q = require('q');
 var async = require('async');
+var moment = require('moment');
 
+// Load data from pgsql database
 var LoadCallCenterPerformanceData = function(tenant, company, startTime, endTime){
     var deferred = Q.defer();
 
@@ -16,7 +18,7 @@ var LoadCallCenterPerformanceData = function(tenant, company, startTime, endTime
     {
 
         var callCenterPerformanceQuery = {
-            where : [{Company: company, Tenant: tenant, WindowName: {$in:['CALLS', 'QUEUE', 'QUEUEANSWERED', 'CONNECTED', 'QUEUEDROPPED', 'LOGIN', 'AFTERWORK']}, SummaryDate: {between:[startTime, endTime]}}]
+            where : [{Company: company, Tenant: tenant, WindowName: {$in:['CALLS', 'QUEUE', 'QUEUEANSWERED', 'CONNECTED', 'QUEUEDROPPED', 'LOGIN', 'AFTERWORK', 'BREAK', 'AGENTHOLD']}, SummaryDate: {between:[startTime, endTime]}}]
         };
 
         dbConn.DashboardDailySummary.findAll(callCenterPerformanceQuery).then(function(performanceData)
@@ -36,6 +38,7 @@ var LoadCallCenterPerformanceData = function(tenant, company, startTime, endTime
     return deferred.promise;
 };
 
+// data processing functions
 var ProcessCalls = function(performanceData){
     var deferred = Q.defer();
 
@@ -119,7 +122,6 @@ var ProcessQueueAnswer = function(performanceData){
     return deferred.promise;
 };
 
-
 var ProcessAnswer = function(performanceData){
     var deferred = Q.defer();
 
@@ -130,7 +132,8 @@ var ProcessAnswer = function(performanceData){
             TotalOutboundAgentCount: 0,
             TotalInboundAnswerCount: 0,
             TotalOutboundAnswerCount: 0,
-            TotalAnswerTime: 0
+            TotalInboundAnswerTime: 0,
+            TotalOutboundAnswerTime: 0
         };
 
         var answerData = performanceData.filter(function (pData) {
@@ -138,14 +141,15 @@ var ProcessAnswer = function(performanceData){
         });
 
         answerData.forEach(function (answer) {
-            result.TotalAnswerTime = result.TotalAnswerTime + answer.TotalTime;
 
             if(answer.Param2 === "CALLinbound"){
                 result.TotalInboundAnswerCount = result.TotalInboundAnswerCount + answer.TotalCount;
                 result.TotalInboundAgentCount = result.TotalInboundAgentCount + 1;
+                result.TotalInboundAnswerTime = result.TotalInboundAnswerTime + answer.TotalTime;
             }else{
                 result.TotalOutboundAnswerCount = result.TotalOutboundAnswerCount + answer.TotalCount;
                 result.TotalOutboundAgentCount = result.TotalOutboundAgentCount + 1;
+                result.TotalOutboundAnswerTime = result.TotalOutboundAnswerTime + answer.TotalTime;
             }
         });
 
@@ -238,55 +242,156 @@ var ProcessAfterWork = function(performanceData){
     return deferred.promise;
 };
 
+var ProcessBreak = function(performanceData){
+    var deferred = Q.defer();
 
+    try{
+
+        var result = {
+            TotalBreakTime: 0
+        };
+
+        var acwData = performanceData.filter(function (pData) {
+            return pData.WindowName === "BREAK";
+        });
+
+        acwData.forEach(function (acw) {
+            result.TotalBreakTime = result.TotalBreakTime + acw.TotalTime;
+        });
+
+        deferred.resolve(result);
+
+    }catch(ex){
+        deferred.reject(ex);
+    }
+
+    return deferred.promise;
+};
+
+var ProcessHold = function(performanceData){
+    var deferred = Q.defer();
+
+    try{
+
+        var result = {
+            TotalHoldTime: 0
+        };
+
+        var acwData = performanceData.filter(function (pData) {
+            return pData.WindowName === "AGENTHOLD";
+        });
+
+        acwData.forEach(function (acw) {
+            result.TotalHoldTime = result.TotalHoldTime + acw.TotalTime;
+        });
+
+        deferred.resolve(result);
+
+    }catch(ex){
+        deferred.reject(ex);
+    }
+
+    return deferred.promise;
+};
+
+// Calculate performance summary for one date
+var GetSingleDateSummary = function (filterDataForDate, summaryDate) {
+
+    var deferred = Q.defer();
+
+    var asyncTasks = [
+        ProcessCalls(filterDataForDate),
+        ProcessQueues(filterDataForDate),
+        ProcessAnswer(filterDataForDate),
+        ProcessDropped(filterDataForDate),
+        ProcessLogin(filterDataForDate),
+        ProcessAfterWork(filterDataForDate),
+        ProcessQueueAnswer(filterDataForDate),
+        ProcessBreak(filterDataForDate),
+        ProcessHold(filterDataForDate)
+    ];
+
+    Q.all(asyncTasks).then(function (results) {
+        var callCenterPerformance = {
+            totalInbound: results[0].TotalInboundCallCount,
+            totalOutbound: results[0].TotalOutboundCallCount,
+            totalQueued: results[1].TotalQueued,
+            totalQueueAnswered: results[6].TotalQueueAnswered,
+            totalQueueDropped: results[3].TotalDropped,
+            totalStaffTime: results[4].TotalStaffTime,
+            totalAcwTime: results[5].TotalAcwTime,
+            AverageStaffTime: results[4].TotalStaffCount?results[4].TotalStaffTime / results[4].TotalStaffCount: 0,
+            AverageAcwTime: results[4].TotalStaffCount?results[5].TotalAcwTime / results[4].TotalStaffCount: 0,
+            AverageInboundCallsPerAgent: results[2].TotalInboundAgentCount?results[2].TotalInboundAnswerCount/ results[2].TotalInboundAgentCount: 0,
+            AverageOutboundCallsPerAgent: results[2].TotalOutboundAgentCount?results[0].TotalOutboundCallCount / results[2].TotalOutboundAgentCount: 0,
+            TotalStaffCount: results[4].TotalStaffCount,
+
+
+            totalTalkTimeInbound: results[2].TotalInboundAnswerTime,
+            totalTalkTimeOutbound: results[2].TotalOutboundAnswerTime,
+            totalBreakTime: results[7].TotalBreakTime,
+            totalHoldTime: results[8].TotalHoldTime,
+            AverageTalkTimeInbound: results[0].TotalInboundCallCount? results[2].TotalInboundAnswerTime / results[0].TotalInboundCallCount: 0,
+            AverageTalkTimeOutbound: results[0].TotalOutboundCallCount? results[2].TotalOutboundAnswerTime / results[0].TotalOutboundCallCount: 0
+        };
+
+        deferred.resolve({Date:summaryDate, SummaryData: callCenterPerformance});
+        //callCenterPerformanceSummary.push({Date:m_startTime, SummaryDate: callCenterPerformance})
+        //callback(undefined, callCenterPerformance);
+    }).catch(function(err) {
+        deferred.resolve({Date:summaryDate, SummaryData: undefined});
+    });
+
+    return deferred.promise;
+
+};
+
+// Initiate performance calculation
 var GetCallCenterPerformance = function (tenant, company, startTime, endTime, callback) {
 
-    var callCenterPerformance = {
+    /*var callCenterPerformance = {
         totalInbound: 0,
         totalOutbound: 0,
         totalQueued: 0,
         totalQueueAnswered: 0,
         totalQueueDropped: 0,
-        totalTalkTime: 0,
         totalStaffTime: 0,
         totalAcwTime: 0,
         AverageStaffTime: 0,
         AverageAcwTime: 0,
         AverageInboundCallsPerAgent: 0,
         AverageOutboundCallsPerAgent: 0,
-        TotalLoginAgents: 0
-    };
+        TotalStaffCount: 0,
+
+        totalTalkTimeInbound: 0,
+        totalTalkTimeOutbound: 0,
+        totalBreakTime: 0,
+        totalIdleTime: 0,
+        totalHoldTime: 0,
+        AverageTalkTimeInbound: 0,
+        AverageTalkTimeOutbound: 0
+    };*/
+
+    var callCenterPerformanceSummary = [];
 
     LoadCallCenterPerformanceData(tenant, company, startTime, endTime).then(function(performanceData){
 
-        var asyncTasks = [
-            ProcessCalls(performanceData),
-            ProcessQueues(performanceData),
-            ProcessAnswer(performanceData),
-            ProcessDropped(performanceData),
-            ProcessLogin(performanceData),
-            ProcessAfterWork(performanceData),
-            ProcessQueueAnswer(performanceData)
-        ];
+        var summaryTasks = [];
+        var m_endTime = moment(endTime);
+        for (var m_startTime = moment(startTime); m_startTime.isBefore(m_endTime); m_startTime.add(1, 'days')) {
+            var filterDate = m_startTime.format('YYYY-MM-DD');
+            console.log(filterDate);
 
-        Q.all(asyncTasks).then(function (results) {
-            callCenterPerformance = {
-                totalInbound: results[0].TotalInboundCallCount,
-                totalOutbound: results[0].TotalOutboundCallCount,
-                totalQueued: results[1].TotalQueued,
-                totalQueueAnswered: results[6].TotalQueueAnswered,
-                totalQueueDropped: results[3].TotalDropped,
-                totalTalkTime: results[2].TotalAnswerTime,
-                totalStaffTime: results[4].TotalStaffTime,
-                totalAcwTime: results[5].TotalAcwTime,
-                AverageStaffTime: results[4].TotalStaffCount?results[4].TotalStaffTime / results[4].TotalStaffCount: 0,
-                AverageAcwTime: results[4].TotalStaffCount?results[5].TotalAcwTime / results[4].TotalStaffCount: 0,
-                AverageInboundCallsPerAgent: results[2].TotalInboundAgentCount?results[2].TotalInboundAnswerCount/ results[2].TotalInboundAgentCount: 0,
-                AverageOutboundCallsPerAgent: results[2].TotalOutboundAgentCount?results[0].TotalOutboundCallCount / results[2].TotalOutboundAgentCount: 0,
-                TotalStaffCount: results[4].TotalStaffCount
-            };
+            var filterDataForDate = performanceData.filter(function (data) {
+                return filterDate === moment(data.SummaryDate).format('YYYY-MM-DD');
+            });
 
-            callback(undefined, callCenterPerformance);
+            summaryTasks.push(GetSingleDateSummary(filterDataForDate, m_startTime));
+        }
+
+
+        Q.all(summaryTasks).then(function (results) {
+            callback(undefined, results);
         }).catch(function(err) {
             callback(err, undefined);
         });
