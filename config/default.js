e455e398f9fa3e05ba0841b5b159215c922c2723
heshanindika <k.heshan.indika@gmail.com>
2017-07-05 16:00:55 +0530

    change defaultConfig

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 ﻿module.exports = {
 	"Redis":
 	{
-		"mode":"sentinel",//instance, cluster, sentinel
+		"mode":"instance",//instance, cluster, sentinel
 		"ip": "45.55.142.207",
 		"port": 6389,
 		"user": "duo",
@@ -49,7 +49,7 @@
 		"port": 6389,
 		"user": "duo",
 		"password": "DuoS123",
-		"mode":"sentinel",//instance, cluster, sentinel
+		"mode":"instance",//instance, cluster, sentinel
 		"sentinels":{
 			"hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
 			"port":16389,
