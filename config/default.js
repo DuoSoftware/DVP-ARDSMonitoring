23eb649dd68f41af17b12d075789ac71bb410b46
duobcoder <60178186+duobcoder@users.noreply.github.com>
2020-05-16 11:04:27 +0530

    updated env variable values

--- config/default.js
+++ config/default.js
@@ -1,21 +1,21 @@
 ﻿module.exports = {
     "Redis":
         {
-            "mode":"sentinel",//instance, cluster, sentinel
-            "ip": "45.55.142.207",
-            "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
+            "mode":"instace",//instance, cluster, sentinel
+            "ip": "",
+            "port": 6379,
+            "user": "",
+            "password": "",
             "db":6,
             "sentinels":{
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port":16389,
+                "hosts": "",
+                "port":6379,
                 "name":"redis-cluster"
             }
 
         },
     "Services" : {
-        "accessToken":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo",
+        "accessToken":"",
         "routingServiceHost": "ardsliteroutingengine.104.131.67.21.xip.io",
         "routingServicePort": "2223",
         "routingServiceVersion": "1.0.0.0",
@@ -36,32 +36,32 @@
     },
     "DB": {
         "Type":"postgres",
-        "User":"duo",
-        "Password":"DuoS123",
+        "User":"postgres",
+        "Password":"",
         "Port":5432,
-        "Host":"104.236.231.11",//104.131.105.222
+        "Host":"",//104.131.105.222
         "Database":"duo"
     },
     "Security":
         {
 
-            "ip" : "45.55.142.207",
-            "port": 6389,
+            "ip" : "",
+            "port": 6379,
             "user": "duo",
-            "password": "DuoS123",
-            "mode":"sentinel",//instance, cluster, sentinel
+            "password": "",
+            "mode":"instance",//instance, cluster, sentinel
             "sentinels":{
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port":16389,
+                "hosts": "",
+                "port":6379,
                 "name":"redis-cluster"
             }
         },
     "RabbitMQ":
         {
-            "ip": "45.55.142.207",
+            "ip": "",
             "port": 5672,
-            "user": "guest",
-            "password": "guest",
+            "user": "",
+            "password": "",
             "vhost":'/'
         }
 };
